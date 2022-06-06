78e5c4a1201c34db114b5784edd7aeb0ca5bc005
Cameron <weijmer.c@gmail.com>
2022-06-06 14:42:27 +0200

    Open navbar mobile

--- javascript/main.js
+++ javascript/main.js
@@ -24,3 +24,21 @@ function done() {
   clearInterval(clock);
   clock = null;
 }
+
+let menuList = document.getElementById("menuList");
+const content_block = document.getElementById("content_block");
+
+menuList.style.maxHeight = "0px";
+
+// Function to toggle the menulist
+function togglemenu() {
+  if (menuList.style.maxHeight == "0px") {
+    menuList.style.maxHeight = "500px";
+    menuList.style.visibility = "visible";
+    content_block.style.visibility = "hidden";
+  } else {
+    content_block.style.visibility = "visible";
+    menuList.style.visibility = "hidden";
+    menuList.style.maxHeight = "0px";
+  }
+}
