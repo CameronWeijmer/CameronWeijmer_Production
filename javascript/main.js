944a50be6f15bc5a2ad37afe55093a3cf2cb8337
Cameron <weijmer.c@gmail.com>
2022-06-06 14:48:57 +0200

    Fixed navbar

--- javascript/main.js
+++ javascript/main.js
@@ -35,9 +35,7 @@ function togglemenu() {
   if (menuList.style.maxHeight == "0px") {
     menuList.style.maxHeight = "500px";
     menuList.style.visibility = "visible";
-    content_block.style.visibility = "hidden";
   } else {
-    content_block.style.visibility = "visible";
     menuList.style.visibility = "hidden";
     menuList.style.maxHeight = "0px";
   }
