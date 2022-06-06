053004c318dfdc7a9284e5912abd68af6299bed0
Cameron <weijmer.c@gmail.com>
2022-06-06 14:25:05 +0200

    justified code

--- javascript/main.js
+++ javascript/main.js
@@ -6,21 +6,21 @@ let clock = setInterval(onTick, 100);
 
 text.textContent = "";
 for (let i = 0; i < textSplit.length; i++) {
-    text.innerHTML += "<span>" + textSplit[i] + "</span>";
+  text.innerHTML += "<span>" + textSplit[i] + "</span>";
 }
 
 function onTick() {
-    const span = text.querySelectorAll('span')[int];
-    span.classList.add('cover');
-    int++
+  const span = text.querySelectorAll("span")[int];
+  span.classList.add("cover");
+  int++;
 
-    if (int === textSplit.length) {
-        done();
-        return;
-    }
+  if (int === textSplit.length) {
+    done();
+    return;
+  }
 }
 
 function done() {
-    clearInterval(clock);
-    clock = null;
+  clearInterval(clock);
+  clock = null;
 }
