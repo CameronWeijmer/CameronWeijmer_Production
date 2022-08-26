ffb86c1ec8b90c51ce88bb028dbba1ae9865ebd0
Cameron <weijmer.c@gmail.com>
2022-08-26 20:45:56 +0200

    🚀 Added 404

---
+++ javascript/404.js
@@ -0,0 +1,26 @@
+const text = document.querySelector(".name");
+const textStr = text.textContent;
+const textSplit = textStr.split("");
+let int = 0;
+let clock = setInterval(onTick, 800);
+
+text.textContent = "";
+for (let i = 0; i < textSplit.length; i++) {
+  text.innerHTML += "<span>" + textSplit[i] + "</span>";
+}
+
+function onTick() {
+  const span = text.querySelectorAll("span")[int];
+  span.classList.add("cover");
+  int++;
+
+  if (int === textSplit.length) {
+    done();
+    return;
+  }
+}
+
+function done() {
+  clearInterval(clock);
+  clock = null;
+}
